157ee26ba2b147b7e302182ee9a60ba85ff9ea9c
Theodore Chen <tschen@flatbread.local>
2018-10-07 02:26:32 -0400

    beginning work on the angular js code

--- module6/js/app.js
+++ module6/js/app.js
@@ -1,3 +1,15 @@
 (function () {
-    'use strict';
+'use strict';
+
+angular.module("LunchCheck", [])
+.controller(LunchCheckController, function ($scope) {
+    $scope.lunchCheckMessage = "";
+    $scope.lunchMenu = "";
+
+    $scope.calculateLunchCheck = function () {
+        
+    };
+});
+
+LunchCheckController["$ingestor"] = ["$scope"]
 })();
