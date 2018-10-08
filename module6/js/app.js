96b1825705e6c6f98dfbb265723135d0fef71f01
Theodore Chen <tschen@flatbread.local>
2018-10-08 02:42:35 -0400

    calculateLunchCheck properly parses and cleans up input

--- module6/js/app.js
+++ module6/js/app.js
@@ -14,7 +14,7 @@ function LunchCheckController ($scope) {
             $scope.lunchCheckMessage = "Please enter data first";
             return;
         }
-        var rawArray = $scope.lunchMenu.split(',');
+        var rawArray = $scope.lunchMenu.replace(/\s/g,'').split(',');
         var lunchMenuItems = [];
         rawArray.reduce( (accumulator, currentValue) => {
             if (currentValue != "") {
@@ -22,8 +22,10 @@ function LunchCheckController ($scope) {
             }
             return accumulator;
         }, lunchMenuItems);
-        console.log(lunchMenuItems);
-        if (lunchMenuItems.length <= 3) {
+        // console.log(lunchMenuItems);
+        if (lunchMenuItems.length == 0) {
+            $scope.lunchCheckMessage = "No items found";
+        } else if (lunchMenuItems.length <= 3 && lunchMenuItems.length > 0) {
             $scope.lunchCheckMessage = "Enjoy!";
         } else {
             $scope.lunchCheckMessage = "Too Much!";
