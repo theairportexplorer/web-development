7b0432db48e8d06382a866f0ab028d72b9173623
Theodore Chen <tschen@flatbread.local>
2018-10-08 02:25:18 -0400

    beginning work on js code

--- module6/js/app.js
+++ module6/js/app.js
@@ -2,14 +2,32 @@
 'use strict';
 
 angular.module("LunchCheck", [])
-.controller(LunchCheckController, function ($scope) {
+.controller("LunchCheckController", LunchCheckController);
+
+LunchCheckController.$ingest = ["$scope"];
+function LunchCheckController ($scope) {
     $scope.lunchCheckMessage = "";
     $scope.lunchMenu = "";
 
     $scope.calculateLunchCheck = function () {
-        
+        if ($scope.lunchMenu == "") {
+            $scope.lunchCheckMessage = "Please enter data first";
+            return;
+        }
+        var rawArray = $scope.lunchMenu.split(',');
+        var lunchMenuItems = [];
+        rawArray.reduce( (accumulator, currentValue) => {
+            if (currentValue != "") {
+                accumulator.push(currentValue);
+            }
+            return accumulator;
+        }, lunchMenuItems);
+        console.log(lunchMenuItems);
+        if (lunchMenuItems.length <= 3) {
+            $scope.lunchCheckMessage = "Enjoy!";
+        } else {
+            $scope.lunchCheckMessage = "Too Much!";
+        }
     };
-});
-
-LunchCheckController["$ingestor"] = ["$scope"]
+}
 })();
