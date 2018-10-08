cc1a78c3c612c923ea1859a030f2ba20b246f2b6
Theodore Chen <tschen@flatbread.local>
2018-10-08 12:08:30 -0400

    implemented "bonus" options for jhu requirements

--- module6/js/app.js
+++ module6/js/app.js
@@ -8,10 +8,15 @@ LunchCheckController.$ingest = ["$scope"];
 function LunchCheckController ($scope) {
     $scope.lunchCheckMessage = "";
     $scope.lunchMenu = "";
+    $scope.lunchMessageStyle = {};
 
     $scope.calculateLunchCheck = function () {
         if ($scope.lunchMenu == "") {
             $scope.lunchCheckMessage = "Please enter data first";
+            $scope.lunchMessageStyle = {
+                'border': '2px solid red',
+                'color': 'red'
+            };
             return;
         }
         var rawArray = $scope.lunchMenu.replace(/\s/g,'').split(',');
@@ -25,10 +30,22 @@ function LunchCheckController ($scope) {
         // console.log(lunchMenuItems);
         if (lunchMenuItems.length == 0) {
             $scope.lunchCheckMessage = "No items found";
+            $scope.lunchMessageStyle = {
+                'border': '2px solid red',
+                'color': 'red'
+            };
         } else if (lunchMenuItems.length <= 3 && lunchMenuItems.length > 0) {
             $scope.lunchCheckMessage = "Enjoy!";
+            $scope.lunchMessageStyle = {
+                'border': '2px solid green',
+                'color': 'green'
+            };
         } else {
             $scope.lunchCheckMessage = "Too Much!";
+            $scope.lunchMessageStyle = {
+                'border': '2px solid green',
+                'color': 'green'
+            };
         }
     };
 }
